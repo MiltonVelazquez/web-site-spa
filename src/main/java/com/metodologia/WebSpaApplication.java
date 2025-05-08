8c4ae3b6f3641548f0898f627bea2771278e3c8b
MiltonVelazquez <vmilton250@gmail.com>
2025-05-08 16:36:47 -0300

    Despliegue

--- src/main/java/com/metodologia/WebSpaApplication.java
+++ src/main/java/com/metodologia/WebSpaApplication.java
@@ -1,6 +1,6 @@
 package com.metodologia;
 
-import or.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
